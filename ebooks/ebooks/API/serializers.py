1bce3baa72c09d61b7adcb90427c8d59975b85fa
mlobf <marcoslemeborbafilho@gmail.com>
2021-05-02 23:55:48 -0300

    First Commit

---
+++ ebooks/ebooks/API/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from ebook.models import Ebook, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Review
+        fields = "__all__"
+
+
+class EbookSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        models = Ebook
+        fields = "__all__"
